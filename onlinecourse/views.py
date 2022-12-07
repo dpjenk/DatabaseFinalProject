7456fc93abe6cbf02801a8217604d9ba72bcdc6a
dpjenk <dpjenk@outlook.com>
2022-12-06 19:05:21 -0500

    Lab4 final commit

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -123,13 +123,13 @@ def submit(request, course_id):
 
 # <HINT> A example method to collect the selected choices from the exam form from the request object
 def extract_answers(request):
-    submitted_anwsers = []
+    submitted_answers = []
     for key in request.POST:
         if key.startswith('choice'):
             value = request.POST[key]
             choice_id = int(value)
-            submitted_anwsers.append(Choice.objects.get(id=choice_id))
-    return submitted_anwsers
+            submitted_answers.append(Choice.objects.get(id=choice_id))
+    return submitted_answers
 
 
 # <HINT> Create an exam result view to check if learner passed exam and show their question results and result for each question,
